aae62d291b846f060faef13407d4aeb1002cc542
PedroSZ <solano_zepeda@hotmail.com>
2021-12-02 18:43:04 -0600

    Single page aplication part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PromotionService } from './services/promotion.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,7 +43,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule,
     FlexLayoutModule
   ],
-  providers: [DishService],
+  providers: [DishService,
+  PromotionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
